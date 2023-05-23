687b9ca9eba3031430d30a1aa164f73a53e114e0
vladpobol <pobol.email@yandex.ru>
2023-05-23 15:25:58 +0300

    save control and test catboost models

--- connect_database.py
+++ connect_database.py
@@ -1,16 +1,19 @@
 from sqlalchemy import Column, Integer, String, DateTime, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+import psycopg2
+import numpy as np
+import pandas as pd
 
-SQLALCHEMY_DATABASE_URL = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
+DATABASE_URI = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
 
-# создаём engine
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size = 100, max_overflow = 100)
+engine = create_engine(DATABASE_URI)
 # настройка класса Session c требуемыми настройками
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+#создаем классы наших таблиц
 class User_data(Base):
     __tablename__ = 'user_data'
     user_id = Column(Integer,primary_key=True)
@@ -37,5 +40,41 @@ class Feed_data(Base):
     action = Column(String)
     target = Column(Integer)
 
+def get_data_with_psycopg(query: str):
+    '''с помощью SQL запроса через библиотеку pcycopg2
+возвращает резульат запроса в виде numpy.ndarray'''
+
+    conn = psycopg2.connect(DATABASE_URI)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return np.array(result)
+
+def get_data_with_sqlalchemy(table: str, limit):
+    '''С помощью SQLAlchemy и указанием таблица и лимита
+возвращает таблицу из базы с заданным лимитом
+     
+     user == user_data
+     post == posts
+     feed == feed_data'''
+
+    table_dict = {
+            'user': User_data,
+            'post': Post,
+            'feed': Feed_data
+            }
+    with SessionLocal() as session:
+        data_from_db = session.query(table_dict[table]).limit(limit).all()
+
+    return pd.DataFrame([item.__dict__ for item in data_from_db]).iloc[:,1:]# в первой колонке находятся экземпляры класса нашей таблицы, выкидываем их и оставляем только фичи
+
+
+
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
+
+print(type(get_data_with_psycopg('SELECT * from pobol_user10_features')))
